4b9cbd45b8409b0d62105f82b20b88d8450bf294
ww18 <476848607@qq.com>
2015-09-24 18:39:10 +0800

    change structure and the scss comprass

--- app/app.js
+++ app/app.js
@@ -3,19 +3,19 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.myBlog',
+  'myApp.market',
   'myApp.myProfile',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
       $routeProvider
-          .when('/myBlog', {
-            templateUrl: 'my-blog/my-blog.html',
-            controller: 'myBlog'
+          .when('/market', {
+            templateUrl: 'market/market.html',
+            controller: 'market'
           })
           .when('/myProfile', {
             templateUrl: 'my-profile/my-profile.html',
             controller: 'myProfile'
           })
-          .otherwise({redirectTo: '/myBlog'});
+          .otherwise({redirectTo: '/market'});
 }]);
--- app/my-blog/my-blog.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by phoebe on 15/9/7.
- */
-'use strict';
-
-angular.module('myApp.myBlog', [])
-    .controller('myBlog', [function() {
-
-    }]);
